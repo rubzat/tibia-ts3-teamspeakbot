59e69172173d807e481b1e842ed714a6bd7a716b
rubzat <rubzat@gmail.com>
2021-09-22 04:48:17 -0600

    update

--- src/api/crone/lists.js
+++ src/api/crone/lists.js
@@ -134,7 +134,8 @@ const getNotPokedKills = async (kills = []) => (
 const mapCharactersToNames = ({ type, characterName }) => ({ type, characterName });
 
 export const startTasks = (teamspeak) => {
-  const listTask = cron.schedule('*/1 * * * * *', async () => {
+  const listTask = cron.schedule('0-59/5 * * * * *', async () => {
+    console.log('Init')
     const enemyCharacters = await Characters.find({ type: 'enemy' });
     const friendCharacters = await Characters.find({ type: 'friend' });
     const neutralCharacters = await Characters.find({ type: 'neutral' });
